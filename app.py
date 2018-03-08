2f9400a10fa424a4e51ae5caffa1311d9f8e051b
Tacio S. S <tacioss.br@gmail.com>
2018-03-08 18:54:25 -0300

    add cadastro de url

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+import os
+import random
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+mongo = PyMongo(app)
 
-@app.route('/')
-def hello_world():
-    return "Hello, world"
+@app.route('/new/<url>')
+def hello_world(url):
+
+    if url.startswith("https://") or url.startswith("http://"):
+        new = {
+            'original_url': url,
+            'short_url': int(random.random()*100000000)
+        }
+        print(new)
+        mongo.db.urls.insert(new)
+        return "Hello, world"
+    
+    return str({
+        'error': "Wrong url format, make sure you have a valid protocol and real site."
+    })
