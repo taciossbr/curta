e527aa4dfce88e800be69b99a4c23a78dd14df45
Tacio S. S <tacioss.br@gmail.com>
2018-03-09 19:41:15 -0300

    change mongo to sqlite

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 import os
 import random
+import string
 from flask import Flask, request
-from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-mongo = PyMongo(app)
-app.config['MONGO_URI'] = os.environ['MONGODB_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///curta.db'
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class URL(db.Model):
+
+    short_url = db.Column(db.String(8), primary_key=True)
+    original_url = db.Column(db.String(256))
+    def __repr__(self):
+        return '<URL short_url=%r, original_url%r>' % (short_url, original_url)
+db.create_all()
+def generate_code_link():
+    conj = string.ascii_letters + string.digits
+    s = ''
+    for i in range(8):
+        s+= random.choice(conj)
+    return s
 
 @app.route('/new/<path:url>', methods=['GET'])
-def hello_world(url):
-
+def new(url):
     if url.startswith("https://") or url.startswith("http://"):
-        new = {
-            'original_url': url,
-            'short_url': int(random.random()*100000000)
-        }
-        mongo.db.urls.insert(new)
+        while True:
+            try:
+                rand = generate_code_link()
+                new = URL(short_url=rand, original_url=url)
+                db.session.add(new)
+                db.session.commit()
+                break
+            except:
+                pass
         r = {
-            'original_url': new['original_url'],
-            'short_url': request.url.split('new/')[0] + str(new['short_url'])
+            'original_url': new.original_url,
+            'short_url': new.short_url
         }
-        print(r)
         return str(r)
-    
     return str({
         'error': "Wrong url format, make sure you have a valid protocol and real site."
     })
